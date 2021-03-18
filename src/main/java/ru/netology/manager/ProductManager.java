e82e56602086a1e7c1338c78e884ecc110cfff59
hYxA <hYxA@users.noreply.github.com>
2021-03-18 17:13:54 +0300

    Added ProductManager

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Product;
+
+public class ProductManager {
+    // добавьте необходимые поля, конструкторы и методы
+
+    public Product[] searchBy(String text) {
+        // ваш код
+    }
+
+    public boolean matches(Product product, String search) {
+        // ваш код
+    }
+}
