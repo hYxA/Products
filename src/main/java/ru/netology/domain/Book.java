e603c92db4a60885efa88c9f8b52540450cea417
hYxA <hYxA@users.noreply.github.com>
2021-03-18 15:54:48 +0300

    Edit class Book

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -1,5 +1,7 @@
 package ru.netology.domain;
 
+import java.util.Objects;
+
 public class Book extends Product{
     private String author;
     private int pages;
@@ -39,4 +41,28 @@ public class Book extends Product{
     public void setPublishedYear(int publishedYear) {
         this.publishedYear = publishedYear;
     }
-}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return pages == book.pages &&
+                publishedYear == book.publishedYear &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), author, pages, publishedYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", pages=" + pages +
+                ", publishedYear=" + publishedYear +
+                '}';
+    }}
