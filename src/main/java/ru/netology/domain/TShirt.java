069ad4f49e704b22f6e44a9d276cc7cd83af5dca
hYxA <hYxA@users.noreply.github.com>
2021-03-18 16:20:49 +0300

    Edit TShirt class

--- src/main/java/ru/netology/domain/TShirt.java
+++ src/main/java/ru/netology/domain/TShirt.java
@@ -1,14 +1,27 @@
+
 package ru.netology.domain;
 
-public class TShirt extends Product{
+import java.util.Objects;
+
+public class TShirt extends Product {
     private String color;
     private String size;
 
-    public TShirt(int id, String name, int price, String color, String size) {
-        super(id, name, price);
-        this.color = color;
-        this.size = size;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        TShirt shirt = (TShirt) o;
+        return Objects.equals(color, shirt.color) &&
+                Objects.equals(size, shirt.size);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), color, size);
+    }
+
 
     public TShirt() {
     }
