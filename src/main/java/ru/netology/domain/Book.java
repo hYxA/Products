fa100be937250f45ec5da11a21d75780db4b59f0
hYxA <hYxA@users.noreply.github.com>
2021-03-18 17:10:04 +0300

    Added Lombok to classes

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -1,19 +1,20 @@
 package ru.netology.domain;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-public class Book extends Product{
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+
+public class Book extends Product {
     private String author;
-
-
-
     private String title;
     private int pages;
     private int publishedYear;
-
-    public Book() {
-        super();
-    }
 
     public Book(int id, String name, int price, String author, int pages, int publishedYear) {
         super(id, name, price);
@@ -21,58 +22,4 @@ public class Book extends Product{
         this.pages = pages;
         this.publishedYear = publishedYear;
     }
-
-    public String getTitle() {    return title;    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
-    public int getPublishedYear() {
-        return publishedYear;
-    }
-
-    public void setPublishedYear(int publishedYear) {
-        this.publishedYear = publishedYear;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Book book = (Book) o;
-        return pages == book.pages &&
-                publishedYear == book.publishedYear &&
-                Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), author, pages, publishedYear);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "author='" + author + '\'' +
-                ", pages=" + pages +
-                ", publishedYear=" + publishedYear +
-                '}';
-    }}
+}
--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -1,31 +1,25 @@
 package ru.netology.domain;
 
-public class Smartphone {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+
+public class Smartphone extends Product {
 
     private String model;
     private String manufacturer;
 
-    public Smartphone() {
-    }
-
-    public Smartphone(String model, String manufacturer) {
+    private Smartphone(int id, String name, int price, String model, String manufacturer) {
+        super(id, name, price);
         this.model = model;
         this.manufacturer = manufacturer;
     }
 
-    public String getModel() {
-        return model;
-    }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getManufacturer() {
-        return manufacturer;
-    }
-
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
 }
