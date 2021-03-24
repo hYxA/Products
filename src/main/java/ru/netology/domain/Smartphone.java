2aa7c7d5c914af62553cf8e7cdf5e9c58f851a0d
hYxA <hYxA@users.noreply.github.com>
2021-03-24 16:47:06 +0300

    added constructor

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -15,7 +15,7 @@ public class Smartphone extends Product {
     private String model;
     private String manufacturer;
 
-    private Smartphone(int id, String name, int price, String model, String manufacturer) {
+    public Smartphone(int id, String name, int price, String model, String manufacturer) {
         super(id, name, price);
         this.model = model;
         this.manufacturer = manufacturer;
